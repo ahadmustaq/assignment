ea86dafa03c47907705a87e2db49efdc2acd5def
ahad <ahadmustaq7@gmail.com>
2021-09-29 21:01:28 +0530

    c#assgn

---
+++ 20483_c#assignment/2hintnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sampleprogram
+{
+    class hintnum
+    {
+      public  static void Main()
+        {
+            Console.WriteLine("enter day");
+            string day = Console.ReadLine();
+            int n = int.Parse(day);
+            if (n == 0 || n > 7)
+            {
+                Console.WriteLine("enter valid input ");
+            }
+            else if (n<6)
+            {
+                Console.WriteLine("working day");
+            }
+            else
+                Console.WriteLine("holiday");
+        }
+    }
+}
+
+        
+    
---
+++ 20483_c#assignment/6prime number.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sampleprogram
+{
+	class prime_number
+	{
+		public static void Main(string[] args)
+		{
+			int i, j, p;
+			int[] arr = new int[5];
+
+			Console.Write("Enter elements:");
+			for (i = 0; i < arr.Length; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			Console.Write("Prime list :");
+			for (i = 0; i < arr.Length; i++)
+			{
+				j = 2;
+				p = 1;
+				while (j < arr[i])
+				{
+					if (arr[i] % j == 0)
+					{
+						p = 0;
+						break;
+					}
+					j++;
+				}
+
+				if (p == 1)
+				{
+					Console.Write(arr[i] + " ");
+
+				}
+				if(arr[0] < arr[i]) 
+				{
+					arr[0] = arr[i];
+					Console.Write(arr[i] + " ");
+
+				}
+
+			}
+		}
